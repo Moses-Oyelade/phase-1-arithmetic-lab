45f8c0de14679aa7abf84d58806c1c4069d14b3a
Moses <moses.oyelade@student.moringaschool.com>
2023-08-03 15:09:59 +0100

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+
+const multiply = num1 * num2
+
+//generate a random integer
+let random = Math.ceil(Math.random() * 10);
+
+//Mod to have a reminder 4
+const num3 = 14;
+const num4 = 10;
+
+let mod = num3 % num4;
+
+//max to find the higest number to be 20
+ const max = Math.max(2, 4, 5, 7, 20, 15, 11);
+
